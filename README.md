53da271f7f88f0cf89a85530637f61094a414eb4
Michael Savage <mikejsavage@gmail.com>
2019-03-22 14:58:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+wsd is a tiny websocket termination proxy.
+
+# Requirements
+
+Install nodejs
+
+# Running
+
+Fill in the port/destination variables at the top of wsd.js, then
+
+```
+node wsd.js
+```
---
+++ wsd.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+let listen_port = 12345;
+let destination_host = "google.com";
+let destination_port = 54321;
+
+process.env[ "NODE_PATH" ] = __dirname;
+require( "module" ).Module._initPaths();
+
+let ws = require( "ws" );
+let net = require( "net" );
+
+let server = new ws.Server( { port: listen_port } );
+
+server.on( "connection", function( src ) {
+	let dest = net.connect( destination_port, destination_host );
+
+	src.on( "close", () => dest.destroy() );
+	src.on( "error", () => dest.destroy() );
+	dest.on( "close", () => src.close() );
+	dest.on( "error", () => src.close() );
+
+	dest.on( "connect", function() {
+		src.on( "message", data => dest.write( data ) );
+		dest.on( "data", data => src.send( data.toString( "binary" ) ) );
+	} );
+} );
